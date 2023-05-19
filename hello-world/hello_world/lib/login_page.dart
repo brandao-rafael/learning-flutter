0e3ff01d2f960a79c531be8b0efd990408e713ce
Rafael <rafaelbrandao1992@gmail.com>
2023-05-19 15:01:26 -0300

    style: for login page

--- hello-world/hello_world/lib/login_page.dart
+++ hello-world/hello_world/lib/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/home_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -12,65 +11,86 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
+  Widget _body() {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(height: 100),
+                Container(
+                  height: 100,
+                  margin: const EdgeInsets.all(8.0),
+                  child: Image.asset('assets/images/logo.png'),
+                ),
+                const SizedBox(height: 58),
+                Card(
+                    child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    children: [
+                      TextField(
+                        onChanged: (text) {
+                          email = text;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: "Email",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      Container(height: 10),
+                      TextField(
+                        onChanged: (text) {
+                          password = text;
+                        },
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: "Password",
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            if (email == 'rafaelbrandao1992@gmail.com' &&
+                                password == '123456789') {
+                              // Navigator.of(context).pushReplacement(
+                              //   MaterialPageRoute(builder: (context) => HomePage())
+                              // );
+                              Navigator.of(context)
+                                  .pushReplacementNamed('/home');
+                            }
+                          },
+                          child: const SizedBox(
+                              width: double.infinity,
+                              child: Text(
+                                'Entrar',
+                                textAlign: TextAlign.center,
+                              ))),
+                    ],
+                  ),
+                )),
+              ]),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
+        body: Stack(
+      children: [
+        SizedBox(
+            height: MediaQuery.of(context).size.height,
             child:
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center, 
-                children: [
-                  Container(height: 84),
-                  Container(
-                    height: 50,
-                    margin: const EdgeInsets.all(28.0),
-                    child: Image.asset(
-                        'assets/images/logo.png'
-                    ),
-                  ),
-                  TextField(
-                    onChanged: (text) {
-                      email = text;
-                    },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      labelText: "Email",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  Container(height: 10),
-                  TextField(
-                    onChanged: (text) {
-                      password = text;
-                    },
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      labelText: "Password",
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  Container(height: 10),
-                  ElevatedButton(
-                      onPressed: () {
-                        if (email == 'rafaelbrandao1992@gmail.com' &&
-                            password == '123456789') {
-                          // Navigator.of(context).pushReplacement(
-                          //   MaterialPageRoute(builder: (context) => HomePage())
-                          // );
-                          Navigator.of(context).pushReplacementNamed('/home');
-                        }
-                      },
-                      child: const Text('Entrar')),
-            ]),
-          ),
-        ),
-      ),
-    );
+                Image.asset('assets/images/background.png', fit: BoxFit.cover)),
+        Container(color: Colors.black.withOpacity(0.4)),
+        _body(),
+      ],
+    ));
   }
 }
